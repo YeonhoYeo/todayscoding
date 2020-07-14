c11017196069b5f15d5e6e63dff67f5134757479
yeonhoyeo <yoyonho81@gmail.com>
2020-07-14 11:32:13 -0400

    2019-03-10 추가

---
+++ test.js
@@ -0,0 +1,14 @@
+const inputVal1 = [-1, 3, -1, 5];
+const inputVal2 = [-5, -3, -1];
+const inputVal3 = [2, 4, -2, -3, 8];
+
+function calMaxVal(inputVal1) {
+  let maxVal = 0;
+  for (let i = 0; i < inputVal1.size; i += 1) {
+    if (i==0){
+
+    }
+  }
+}
+
+document.write(calMaxVal(inputVal1));
